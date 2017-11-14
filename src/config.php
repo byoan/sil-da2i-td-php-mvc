cc5c1fe99de1cd82705234c1906673c608b947e5
Yoan Ballesteros <y.ballesteros@presta-module.com>
2017-11-14 10:43:44 +0100

    Export database configuration in a config.php file

---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('_DB_HOST_', 'localhost');
+define('_DB_DATABASE_', 'moviesWebsite');
+define('_DB_USER_', 'root');
+define('_DB_PASSWORD_', 'root');
+define('_DB_DSN_', 'mysql:host=' . _DB_HOST_ . ';dbname=' . _DB_DATABASE_ . ';charset=utf8');
+
+define('_DEBUG_', false);
+
+if (_DEBUG_) {
+    ini_set('error_reporting', -1);
+    ini_set('display_errors', 'on');
+}
