7a7447e08ca6c72a0c8c6340ba5b8cdd3bea8d7d
Yoan Ballesteros <y.ballesteros@presta-module.com>
2017-11-14 10:49:55 +0100

    Move images, css and sql in a ressources folder

--- views/head.php
+++ views/head.php
@@ -4,5 +4,5 @@
         <title>Whiplash</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="ressources/styles.css">
     </head>
